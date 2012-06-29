f5995a9ace4ddd41ae0bc0bcea8688e369b4a64b
d-mart <dave.h.martinez@gmail.com>
2012-06-29 13:59:16 -0400

    customized face for paren match

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -21,4 +21,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(show-paren-match ((t (:foreground "dark turquoise" :strike-through "red" :underline t)))))
