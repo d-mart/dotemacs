a79c4f03775320708c5e3be2bb2c53c98c62dadf
d-mart <dave.h.martinez@gmail.com>
2013-01-29 14:04:33 -0500

    Add comment-hint for how to show diffs in magit commit message area

--- .emacs.d/git-setup.el
+++ .emacs.d/git-setup.el
@@ -17,3 +17,19 @@
 (setq auto-mode-alist
       (append '(("COMMIT_EDITMSG$"  . git-commit-mode))
               auto-mode-alist))
+
+;; from https://gist.github.com/4169777
+;; look at diff when writing a commit message
+;; (defun magit-log-show-diff ()
+;;   (interactive)
+;;   (let ((content (magit-cmd-output "git" '("diff" "--cached" "-U5"))))
+;;     (with-current-buffer (get-buffer-create "*vc-diff*")
+;;       (let ((buffer-undo-list t)
+;;             (inhibit-read-only t))
+;;         (erase-buffer)
+;;         (insert content))
+;;       (diff-mode)
+;;       (setq buffer-read-only t)
+;;       (display-buffer (current-buffer)))))
+
+;; (define-key magit-log-edit-mode-map (kbd "C-c C-d") 'magit-log-show-diff)
