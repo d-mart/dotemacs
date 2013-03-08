acfd2e55b5a098d75071edbef83a51f4cb5b8854
d-mart <dave.h.martinez@gmail.com>
2013-03-08 14:52:18 -0500

    Add sudo-edit file

--- .emacs.d/my-misc-utils.el
+++ .emacs.d/my-misc-utils.el
@@ -94,3 +94,9 @@
   (delete-trailing-whitespace)
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max)))
+
+(defun sudo-find-file (file-name)
+  "Like find file, but opens the file as root."
+  (interactive "FSudo Find File: ")
+  (let ((tramp-file-name (concat "/sudo::" (expand-file-name file-name))))
+    (find-file tramp-file-name)))
