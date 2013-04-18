27434f7a96fea6d062cac60585f5e8a4c2aced2a
d-mart <dave.h.martinez@gmail.com>
2013-04-18 17:10:41 -0400

    Add Vagrantfile to ruby-mode auto-alist

--- .emacs.d/ruby-setup.el
+++ .emacs.d/ruby-setup.el
@@ -67,6 +67,7 @@
 (add-to-list 'auto-mode-alist '("Rakefile$"              . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$"             . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile"                . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile"            . ruby-mode))
 ;(add-to-list 'auto-mode-alist '("Gemfile\\(\\ .lock\])?" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$"               . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$"               . ruby-mode))
