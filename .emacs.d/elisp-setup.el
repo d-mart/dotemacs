66eaba812000956696b4f903b995f681801142c1
d-mart <dave.h.martinez@gmail.com>
2013-10-01 13:09:19 -0400

    Diminish some modes.
    * Not in love with either diminishing all the modes in one place,
      or in all the places, diminishing.

--- .emacs.d/elisp-setup.el
+++ .emacs.d/elisp-setup.el
@@ -10,3 +10,8 @@
 
 ;; file modes
 (add-to-list 'auto-mode-alist '("\\.el\\.gz$" . emacs-lisp-mode))
+
+;; Elisp go-to-definition with M-. and back again with M-,
+(autoload 'elisp-slime-nav-mode "elisp-slime-nav")
+(add-hook 'emacs-lisp-mode-hook (lambda () (elisp-slime-nav-mode t)))
+(eval-after-load 'elisp-slime-nav '(diminish 'elisp-slime-nav-mode))
---
+++ .emacs.d/init.d/090-diminish.el
@@ -0,0 +1,5 @@
+;; Diminish modeline clutter
+(require 'diminish)
+(diminish 'abbrev-mode)
+(diminish 'highlight-parentheses-mode)
+(diminish 'undo-tree)
