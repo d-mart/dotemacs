508cf7b313d2ccd447b5da7ba7af78bd3b7ab41f
d-mart <dave.h.martinez@gmail.com>
2014-04-07 14:18:47 -0400

    configure helm and helm-lings

--- .emacs.d/init.d/550-helm.el
+++ .emacs.d/init.d/550-helm.el
@@ -1,4 +1,18 @@
-;(require 'helm)
+(use-package helm
+  :init
+  (progn
+    (use-package helm-config)
+    (use-package helm-swoop)
+    (use-package helm-ls-git)
+    (use-package helm-git-grep)
+    (use-package helm-ag)
+    (use-package helm-helm-commands)
+    (use-package helm-flycheck)
+    (use-package helm-rails)
+    (use-package helm-rb)
+    (use-package helm-themes)
+    )
+  )
 
 ;(helm :sources '(helm-source-findutils)
 ;      :buffer "*helm-findutils*")
