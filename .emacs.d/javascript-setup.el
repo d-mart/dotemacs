0edb2b306b5a2d847405ef98b6921a43ad65e169
d-mart <dave.h.martinez@gmail.com>
2014-10-07 11:54:42 -0400

    Switch to js2-mode (with tweaks)

--- .emacs.d/javascript-setup.el
+++ .emacs.d/javascript-setup.el
@@ -8,9 +8,16 @@
 (defun my-js-mode-keybindings ()
   (define-key javascript-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region))
 
-(add-hook 'js-mode-hook 'my-programming-mode-hook)
-(add-hook 'js-mode-hook 'my-js-mode-settings)
-(add-hook 'js-mode-hook 'my-js-mode-keybindings)
+(custom-set-variables
+ '(js2-basic-offset 2)
+ '(js2-bounce-indent-p nil)
+ '(js2-highlight-level 3)
+ '(js2-mode-indent-ignore-first-tab t)
+)
 
-(add-to-list 'auto-mode-alist '("\\.js$"     . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.js.erb$" . javascript-mode))
+(add-hook 'js2-mode-hook 'my-programming-mode-hook)
+(add-hook 'js2-mode-hook 'my-js-mode-settings)
+(add-hook 'js2-mode-hook 'my-js-mode-keybindings)
+
+(add-to-list 'auto-mode-alist '("\\.js$"     . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
