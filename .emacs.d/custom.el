6be209f413a67a193962931686aa47034222d7f2
d-mart <dave.h.martinez@gmail.com>
2015-05-13 14:31:28 -0400

    saved settings from *Customeize-xyz*

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,16 +6,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ahk-syntax-directory "~/.emacs.d/plugins/ahk-mode-support/")
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
- '(aquamacs-customization-version-id 0 t)
+ '(ansi-term-color-vector
+   [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"] t)
  '(browse-url-browser-function (quote browse-url-firefox))
- '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-enabled-themes (quote (monokai)))
  '(custom-safe-themes
    (quote
-    ("60e70079a187df634db25db4bb778255eaace1ef4309e56389459fb9418b4840" "1c1e6b2640daffcd23b1f7dd5385ca8484a060aec901b677d0ec0cf2927f7cde" "65e05a8630f98308e8e804d3bbc0232b02fe2e8d24c1db358479a85f3356198d" "6e35fcaa342a44064d869940a0b67c409f2150e94dde39e16bd2bd9496629742" "ea0c5df0f067d2e3c0f048c1f8795af7b873f5014837feb0a7c8317f34417b04" "d1fd8fc0f54f6a1d2905eb0a914bc479486680a028b0964cd87f232309b786c5" "dd00495c188318c11cb8469a88254d080f9e8ba65cf105de05e6e1ee1aa88c69" "ffbc43cd2b8c0f11d6093575bd9b34fa95cdc1366939d37aef309b7d90b340fe" "cfde97b1d5ed1770b8e2e1b739611820c3a3e370cbda75d96e78ef2a5f359b27" "be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" "71efabb175ea1cf5c9768f10dad62bb2606f41d110152f4ace675325d28df8bd" "37ef3ff16a63e2732cb2ac3a43a319cb6c70c5069d33db82f6c4758ee2299a94" "36afe64261e1de73fcfadedf154e4bc2c9ec1969bde0c21798d31366897bc4d2" default)))
+    ("4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" default)))
  '(debug-on-error nil)
- '(ediff-diff-options "-w" t)
+ '(ediff-diff-options "-w")
+ '(fci-rule-character-color "#d9d9d9")
  '(fci-rule-color "#383838")
  '(fringe-mode (quote (0)) nil (fringe))
  '(hl-paren-colors
@@ -24,7 +24,12 @@
  '(icicle-mode t)
  '(indicate-buffer-boundaries (quote right))
  '(indicate-empty-lines t)
+ '(magit-use-overlays nil)
+ '(package-selected-packages
+   (quote
+    (elixir-mode planet-theme racket-mode zonokai-theme avy helm-dash helm-mt multi-term marcopolo kerl rpn-calc highlight-indentation apropospriate-theme hydra mellow-theme moe-theme darktooth-theme material-theme swiper ztree zenburn-theme zen-and-art-theme yari yaml-mode wgrep warm-night-theme visual-regexp-steroids vagrant use-package undo-tree twilight-theme twilight-bright-theme twilight-anti-bright-theme toxi-theme swoop sunrise-commander subatomic256-theme string-utils spacegray-theme soothe-theme sml-modeline smex smartparens seti-theme scss-mode sane-term rvm rust-mode ruby-tools ruby-interpolation ruby-electric ruby-block ruby-additional rspec-mode roguel-ike robe rinari reverse-theme restclient rainbow-delimiters railscasts-theme rails-log-mode protobuf-mode projectile-rails pow phoenix-dark-pink-theme phi-search-mc phi-search-dired phi-grep peep-dired pallet noccur neotree naquadah-theme move-line monokai-theme molokai-theme mode-line-debug mc-extras math-at-point markdown-mode+ mark-multiple mark-more-like-this magit-push-remote lush-theme lua-mode lfe-mode key-chord json-mode js2-mode jg-quicknav jabber ir-black-theme info+ impatient-mode iflipb iedit ibuffer-vc ibuffer-tramp ibuffer-git hlinum highlight-parentheses highlight-numbers hide-lines heroku-theme helm-themes helm-rb helm-rails helm-projectile-all helm-projectile helm-ls-git helm-helm-commands helm-git-grep helm-ag haste haml-mode hackernews guru-mode grandshell-theme go-mode gitignore-mode gitconfig-mode git-timemachine git-messenger free-keys flx-ido flatui-theme firebelly-theme fiplr feature-mode expand-region etags-select es-lib elisp-slime-nav edts easy-kill dockerfile-mode dired+ darkmine-theme dark-souls cycle-resize cyberpunk-theme coffee-mode browse-kill-ring bm awk-it auto-yasnippet auto-indent-mode auto-complete-etags anti-zenburn-theme android-mode ag afternoon-theme ace-jump-buffer ace-isearch)))
  '(remote-shell-program "ssh")
+ '(smartparens-global-mode t)
  '(speedbar-supported-extension-expressions
    (quote
     (".wy" ".by" ".[ch]\\(\\+\\+\\|pp\\|c\\|h\\|xx\\)?" ".tex\\(i\\(nfo\\)?\\)?" ".el" ".emacs" ".l" ".lsp" ".p" ".java" ".f\\(90\\|77\\|or\\)?" ".ada" ".p[lm]" ".tcl" ".m" ".scm" ".pm" ".py" ".g" ".s?html" ".ma?k" "[Mm]akefile\\(\\.in\\)?" ".feature" "Rakefile" ".rb" ".yml")))
@@ -35,24 +40,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bm-face ((t (:background "gray20" :box (:line-width 2 :color "DeepSkyBlue1" :style released-button)))))
+ '(bm-face ((t (:box (:line-width 2 :color "DeepSkyBlue1" :style released-button)))))
  '(bm-fringe-face ((t (:box (:line-width 2 :color "DeepSkyBlue1" :style released-button)))))
- '(diff-added ((t (:foreground "#99cc99"))))
- '(diff-context ((t (:inherit diff-changed :foreground "LightGoldenrod3"))))
- '(diff-hunk-header ((t (:background "#393939" :foreground "#6699cc"))))
- '(diff-indicator-added ((t (:inherit diff-removed :foreground "Green"))))
- '(diff-indicator-removed ((t (:inherit diff-removed :foreground "Red"))))
- '(diff-removed ((t (:foreground "#f2777a"))))
  '(flx-highlight-face ((t (:inherit font-lock-variable-name-face :foreground "deep sky blue" :underline t :weight semi-bold))))
  '(font-lock-punctuation ((t (:inherit diff-changed :foreground "plum1"))) t)
- '(fringe ((t (:background "#393939"))))
- '(hl-line ((t (:inherit nil :background "gray24"))))
- '(linum-highlight-face ((t (:inherit default :foreground "yellow1"))))
- '(magit-diff-file-header ((t (:inherit diff-file-header :foreground "sienna2"))))
- '(magit-header ((t (:inherit header-line :foreground "VioletRed2"))))
- '(magit-item-highlight ((t nil)))
- '(magit-section-title ((t (:inherit magit-header :foreground "DeepSkyBlue1"))))
- '(show-paren-match ((t (:background "#6699cc" :foreground "#393939")))))
+ '(woman-bold ((t (:inherit bold :foreground "dodger blue"))))
+ '(woman-italic ((t (:foreground "gold2")))))
 
 ;; don't disable narrow-to-region
 (put 'narrow-to-region 'disabled nil)
