2a71c060733abcc61e14cde5b4a1ada9fdab1f9e
d-mart <dave.h.martinez@gmail.com>
2015-05-13 14:18:10 -0400

    lua-mode settings

--- .emacs.d/lua-setup.el
+++ .emacs.d/lua-setup.el
@@ -1,9 +1,9 @@
 ;;;
 ;;; Configuration and mode customization lua
 ;;;
-
-(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
-
 (add-hook 'lua-mode-hook 'my-programming-mode-hook)
 
+(setq lua-indent-level 2)
+
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'auto-mode-alist '("\\.rockspec$" . lua-mode))
