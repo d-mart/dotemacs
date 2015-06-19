bc0c2e58820aee9f467b0950f44974b0f8e94759
d-mart <dave.h.martinez@gmail.com>
2015-06-19 10:15:24 -0400

    moar helm takeover

--- .emacs.d/init.d/550-helm.el
+++ .emacs.d/init.d/550-helm.el
@@ -1,18 +1,19 @@
 (use-package helm
   :init
   (progn
-    (use-package helm-config)
-    (use-package helm-swoop)
-    (use-package helm-ls-git)
-    (use-package helm-git-grep)
     (use-package helm-ag)
-    (use-package helm-helm-commands)
+    (use-package helm-config)
     (use-package helm-flycheck)
+    (use-package helm-git-grep)
+    (use-package helm-helm-commands)
+    (use-package helm-ls-git)
+    (use-package helm-projectile)
+    (use-package helm-projectile-rails)
     (use-package helm-rails)
     (use-package helm-rb)
     (use-package helm-themes)
-    )
   )
+)
 
 ;(helm :sources '(helm-source-findutils)
 ;      :buffer "*helm-findutils*")
