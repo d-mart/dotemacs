e0f0b11a312a2670d4e0019322af014c1c07d864
d-mart <dave.h.martinez@gmail.com>
2016-02-09 11:19:59 -0500

    update git-commit settings

--- .emacs.d/git-setup.el
+++ .emacs.d/git-setup.el
@@ -10,15 +10,29 @@
 (autoload 'git-commit-mode "git-commit-mode"
           "git commit message editing mode" t)
 
+;; ------------------
+;; Keyboard macro to get story number from branch name
+;; in COMMIT_MSG buffers and insert it at the begginging
+;; of the commit comment
+;; ------------------
+(fset 'commit-msg-insert-story-number
+   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([19 98 114 97 110 99 104 32 91 48 45 57 93 13 2 67108896 134217830 17 134217788 91 35 25 93 32] 0 "%d")) arg)))
+
+;(define-key git-commit-mode-map (kbd "C-c m") 'commit-msg-insert-story-number)
+
 (defalias 'mag 'magit-status)
-(defalias 'mgb 'magit-blame-mode)
+(defalias 'mgb 'magit-blame)
 (defalias 'gg  'vc-git-grep)
 
 (setq magit-last-seen-setup-instructions "1.4.0")
 
-(setq auto-mode-alist
-      (append '(("COMMIT_EDITMSG$"  . git-commit-mode))
-              auto-mode-alist))
+;(setq auto-mode-alist
+;      (append '(("COMMIT_EDITMSG$"  . git-commit-mode))
+;              auto-mode-alist))
+
+;; @todo bind git-messenger:popup to an easy key or alias and autoload it
+;; @todo autoload git-timemachine
+
 
 ;; git-gutter-fringe - git diff bitmap markers in margin
 ;(require 'git-gutter-fringe)
