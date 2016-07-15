e3227907ef2bde3f658b05154119051d715a010f
d-mart <dave.h.martinez@gmail.com>
2016-07-15 12:28:34 -0400

    Update js and json settings

--- .emacs.d/javascript-setup.el
+++ .emacs.d/javascript-setup.el
@@ -2,23 +2,24 @@
 ;;; Javascript setup
 ;;; ----------------------
 
-(defun my-js-mode-settings ()
-  (setq js-indent-level 2))
+(defun dm/js-mode-settings ()
+  (custom-set-variables
+   '(js2-basic-offset 2)
+   '(js2-bounce-indent-p nil)
+   '(js2-highlight-level 3)
+   '(js2-mode-indent-ignore-first-tab t)
+   '(js-indent-level 2)))
 
-(defun my-js-mode-keybindings ()
-  ;(define-key javascript-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region)
-)
+(defun dm/js-mode-keybindings ())
 
-(custom-set-variables
- '(js2-basic-offset 2)
- '(js2-bounce-indent-p nil)
- '(js2-highlight-level 3)
- '(js2-mode-indent-ignore-first-tab t)
-)
 
 (add-hook 'js2-mode-hook 'my-programming-mode-hook)
-(add-hook 'js2-mode-hook 'my-js-mode-settings)
-(add-hook 'js2-mode-hook 'my-js-mode-keybindings)
+(add-hook 'js2-mode-hook 'dm/js-mode-settings)
+(add-hook 'js2-mode-hook 'dm/js-mode-keybindings)
+
+(add-hook 'json-mode-hook 'my-programming-mode-hook)
+(add-hook 'json-mode-hook 'dm/js-mode-settings)
+(add-hook 'json-mode-hook 'dm/js-mode-keybindings)
 
 (add-to-list 'auto-mode-alist '("\\.js$"     . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js.erb$" . js2-mode))
