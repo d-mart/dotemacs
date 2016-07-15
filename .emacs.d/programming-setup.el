5ed4f8d7a99f0e47aad6cf83c0bf01a245d83aa3
d-mart <dave.h.martinez@gmail.com>
2016-07-15 12:29:08 -0400

    Add elec-newline advice

--- .emacs.d/programming-setup.el
+++ .emacs.d/programming-setup.el
@@ -9,7 +9,13 @@
   (hl-line-mode 1)
   (rainbow-delimiters-mode t)
   (fontify-at-todo)
-  (highlight-numbers-mode)
-  )
+  (highlight-numbers-mode))
+
+(defadvice electric-newline-and-maybe-indent (before eol-before-electric-newline ())
+  "move to the end of the current line before inserting a new line before
+   instead of breaking the current line"
+  (end-of-line))
+
+(ad-activate 'electric-newline-and-maybe-indent)
 
 ; (projectile-on)
