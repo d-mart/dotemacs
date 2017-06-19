e2e39d17c65b9b4bcf2ba509d672cd1d1013a412
d-mart <dave.h.martinez@gmail.com>
2017-06-19 13:45:03 -0400

    updates to elixir config

--- .emacs.d/elixir-setup.el
+++ .emacs.d/elixir-setup.el
@@ -3,13 +3,15 @@
 ;;; ----------------------
 
 (defun dm/elixir-settings ()
-  (alchemist-made t)
+  (alchemist-mode t)
   (message "loaded dm/elixir-settings"))
 
 (defun dm/elixir-mode-keybindings ()
   (define-key elixir-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region)
   (define-key elixir-mode-map (kbd "C-c #")   'comment-or-uncomment-region)
-  (define-key elixir-mode-map (kbd "C-c C-d") 'dm/elixir-insert-debugger))
+  (define-key elixir-mode-map (kbd "C-c C-d") 'dm/elixir-insert-debugger)
+  (define-key elixir-mode-map (kbd "s-p")     'dm/elixir-insert-inspect)
+  )
 
 (add-hook 'elixir-mode-hook 'my-programming-mode-hook)
 (add-hook 'elixir-mode-hook 'dm/elixir-settings)
@@ -20,6 +22,12 @@
   (beginning-of-line)
   (open-line 1)
   (insert "require IEx; IEx.pry # XXXXXXXXXXXXXXXXXXXXXXXXXXX"))
+
+(defun dm/elixir-insert-inspect ()
+  (interactive)
+  (end-of-line)
+  (newline-and-indent)
+  (insert "|> IO.inspect # XXXXXXXXXXXXXXXXXXXXXXXXXXX"))
 
 (use-package alchemist
   :init
