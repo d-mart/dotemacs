1c74a41cbd3e57533d176c127c737a7d7efbb974
d-mart <dave.h.martinez@gmail.com>
2017-08-30 14:55:46 -0400

    Comment region in sh-mode

--- .emacs.d/sh-setup.el
+++ .emacs.d/sh-setup.el
@@ -7,9 +7,12 @@
   (setq sh-indentation 2)
   (message "Loaded dm-sh-mode-settings"))
 
-(defun dm-sh-mode-keybindings ())
+(defun dm-sh-mode-keybindings ()
+  (define-key sh-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region))
+
 
 (add-hook 'sh-mode-hook 'my-programming-mode-hook)
 (add-hook 'sh-mode-hook 'dm-sh-mode-settings)
+(add-hook 'sh-mode-hook 'dm-sh-mode-keybindings)
 
 (add-to-list 'auto-mode-alist '("\\.envrc$"       . sh-mode))
