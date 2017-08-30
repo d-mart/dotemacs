90b9b0faf51481dafc998a57e210fcc742e0b2c3
d-mart <dave.h.martinez@gmail.com>
2017-08-30 14:56:40 -0400

    find-file-in-project

---
+++ .emacs.d/init.d/725-project-ops.el
@@ -0,0 +1,12 @@
+;; ----------------
+;; Project-level packages, settings
+;; ----------------
+
+; fiplr - fuzzy find in project (e.g. git repo)
+; (global-set-key (kbd "C-x f")   'fiplr-find-file)
+
+(use-package find-file-in-project
+  :init
+  ()
+  :bind
+  (("C-x f" . ffip)))
