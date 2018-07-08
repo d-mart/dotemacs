ede34d0841cac10c8e38517e950d7ca44ebacf8a
d-mart <dave.h.martinez@gmail.com>
2018-07-08 01:40:19 -0400

    two new startup files

---
+++ .emacs.d/init.d/715-version-control.el
@@ -0,0 +1,11 @@
+;; ----------------
+;; version control, git, etc
+;; ----------------
+
+(use-package magit
+  :init
+  ;;;;;;; something like this to hide untracked and stashes:
+  ;;; ;(push (cons [* commitbuf] 'hide) magit-section-initial-visibility-alist)
+  ()
+  :bind
+  ())
