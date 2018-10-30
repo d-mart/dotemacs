1e442b1836677eae01daee08c58b017b7937d0f0
d-mart <dave.h.martinez@gmail.com>
2018-10-30 09:22:52 -0400

    updated plantuml path - mac only

--- .emacs.d/init.d/450-plantuml-mode.el
+++ .emacs.d/init.d/450-plantuml-mode.el
@@ -1,3 +1,5 @@
 (use-package plantuml-mode
   :init
-  (setq plantuml-jar-path "/usr/local/Cellar/plantuml/8057/libexec/plantuml.jar"))
+  ;(setq plantuml-jar-path "/usr/local/Cellar/plantuml/8057/libexec/plantuml.jar")
+  (setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2018.10/libexec/plantuml.jar")
+  )
