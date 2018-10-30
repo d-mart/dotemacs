ed9b7054c851776f42d859fa265c88ea70b185aa
d-mart <dave.h.martinez@gmail.com>
2018-10-30 09:23:19 -0400

    lifted unfill-paragraph

--- .emacs.d/misc-utils.el
+++ .emacs.d/misc-utils.el
@@ -257,3 +257,13 @@ Version 2015-04-09"
       (goto-char -p1))
     (isearch-mode t)
     (isearch-yank-string (buffer-substring-no-properties -p1 -p2))))
+
+; ========================================================================
+  ;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
