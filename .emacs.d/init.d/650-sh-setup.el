714f0b8c2abca82456d9f1d0d329f54925a14845
d-mart <dave.h.martinez@gmail.com>
2018-10-30 09:24:24 -0400

    functions for sending regions or lines to comint buffers

--- .emacs.d/init.d/650-sh-setup.el
+++ .emacs.d/init.d/650-sh-setup.el
@@ -17,3 +17,41 @@
 (add-hook 'sh-mode-hook 'dm-sh-mode-keybindings)
 
 (add-to-list 'auto-mode-alist '("\\.envrc$"       . sh-mode))
+
+;; adpated rom https://stackoverflow.com/questions/6286579/emacs-shell-mode-how-to-send-region-to-shell
+;; note there is some similarity / collision with builtin 'sh-script.el - this is maybe not needed
+(defun sh-send-line-or-region (&optional step)
+  (interactive ())
+  (let ((proc (get-process "shell"))
+        pbuf min max command)
+    (unless proc
+      (let ((currbuff (current-buffer)))
+        (shell)
+        (switch-to-buffer currbuff)
+        (setq proc (get-process "shell"))
+        ))
+    (setq pbuff (process-buffer proc))
+    (if (use-region-p)
+        (setq min (region-beginning)
+              max (region-end))
+      (setq min (point-at-bol)
+            max (point-at-eol)))
+    (setq command (concat (buffer-substring min max) "\n"))
+    (with-current-buffer pbuff
+      (goto-char (process-mark proc))
+      (insert command)
+      (move-marker (process-mark proc) (point))
+      ) ;;pop-to-buffer does not work with save-current-buffer -- bug?
+    (process-send-string  proc command)
+    (display-buffer (process-buffer proc) t)
+    (when step
+      (goto-char max)
+      (next-line))
+    ))
+
+(defun sh-send-line-or-region-and-step ()
+  (interactive)
+  (sh-send-line-or-region t))
+(defun sh-switch-to-process-buffer ()
+  (interactive)
+  (pop-to-buffer (process-buffer (get-process "shell")) t))
