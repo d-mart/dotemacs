0a3e83aff6fbae0328d807395ef507de6d03df12
David Martinez <dm@dmartinez.net>
2019-06-17 20:33:58 -0400

    Update dart mode setup

--- .emacs.d/init.d/650-dart-setup.el
+++ .emacs.d/init.d/650-dart-setup.el
@@ -9,10 +9,18 @@
   (highlight-thing-mode t)
   (message "loaded dm/dart-settings"))
 
+(defun dm/dart-insert-dump ()
+  (interactive)
+  (beginning-of-line)
+  (open-line 1)
+  (insert "debugDumpApp(); // XXXXXXXXXXXXXXXXXXXXXXXXXXX"))
+
 (defun dm/dart-mode-keybindings ()
   (define-key dart-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region)
   (define-key dart-mode-map (kbd "C-c #")   'comment-or-uncomment-region)
-  (define-key dart-mode-map (kbd "C-c h")   'highlight-thing-mode))
+  (define-key dart-mode-map (kbd "C-c h")   'highlight-thing-mode)
+  (define-key dart-mode-map (kbd "C-c i")   'dm/dart-insert-dump)
+  (define-key dart-mode-map (kbd "C-c C-d") 'dm/dart-insert-dump))
 
 (add-hook 'dart-mode-hook 'dm/programming-mode-hook)
 (add-hook 'dart-mode-hook 'dm/dart-settings)
