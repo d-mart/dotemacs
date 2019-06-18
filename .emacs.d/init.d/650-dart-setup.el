dd9047d190df79a45d68b2d662a16c61c52a146f
David Martinez <dm@dmartinez.net>
2019-06-17 20:33:58 -0400

    Update dart mode setup

--- .emacs.d/init.d/650-dart-setup.el
+++ .emacs.d/init.d/650-dart-setup.el
@@ -4,12 +4,15 @@
 
 (defun dm/dart-settings ()
   (sp-local-pair 'dart-mode "<" ">" :wrap "C-<")
+  (setq highlight-thing-case-sensitive-p t)
+  (setq highlight-thing-delay-seconds 3.5)
+  (highlight-thing-mode t)
   (message "loaded dm/dart-settings"))
 
 (defun dm/dart-mode-keybindings ()
   (define-key dart-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region)
   (define-key dart-mode-map (kbd "C-c #")   'comment-or-uncomment-region)
-  )
+  (define-key dart-mode-map (kbd "C-c h")   'highlight-thing-mode))
 
 (add-hook 'dart-mode-hook 'dm/programming-mode-hook)
 (add-hook 'dart-mode-hook 'dm/dart-settings)
