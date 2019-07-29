9c5e1fcdbf55def6c315a76c3dec44a4139cb3da
David Martinez <dm@dmartinez.net>
2019-07-29 09:49:59 -0400

    Combine and clean up dired config files

--- .emacs.d/init.d/200-dired.el
+++ .emacs.d/init.d/200-dired.el
@@ -1,13 +1,24 @@
+;;;------------------
+;;; Dired setup
+;;;------------------
 
-;; adopted from https://stackoverflow.com/questions/19907939/how-can-one-quickly-browse-through-lots-of-files-in-emacs#19908881
 (defun dm/dired-mode-keybindings ()
-  (define-key dired-mode-map (kbd "C-o") 'dired-view-current)     ; was dired-display-file
-  (define-key dired-mode-map (kbd "n")   'dired-view-next)           ; was dired-next-line
-  (define-key dired-mode-map (kbd "p")   'dired-view-previous)) ; was dired-previous-line
+  (define-key dired-mode-map (kbd "C-o")   'dired-view-current)   ; was dired-display-file
+  (define-key dired-mode-map (kbd "n")     'dired-view-next)      ; was dired-next-line
+  (define-key dired-mode-map (kbd "p")     'dired-view-previous)  ; was dired-previous-line
+  (define-key dired-mode-map (kbd "C-c w") 'wdired-change-to-wdired-mode))
 
 (add-hook 'dired-mode-hook 'dm/dired-mode-keybindings)
+(add-hook 'dired-mode-hook 'dm/dired-mode-line-function)
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
 (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+(add-hook 'dired-mode-hook 'auto-revert-mode) ;; Auto-refresh dired on file change
 
+(add-hook 'find-file-hooks 'dm/dired-mode-line-function)
+
+(setq auto-revert-verbose nil) ;; don't be chatty about auto-reverts
+
+;; adopted from https://stackoverflow.com/questions/19907939/how-can-one-quickly-browse-through-lots-of-files-in-emacs#19908881
 (defun dired-view-next ()
   "Move down one line and view the current file in another window."
   (interactive)
@@ -37,6 +48,42 @@
         (view-file file))                                                    ; ... view it
       (other-window -1))))                   ; give the attention back to the dired buffer
 
+;;; from emacswiki - show dired buffers with directories listed first
+(defun dm/dired-sort ()
+  "Sort dired listings with directories first."
+  (save-excursion
+    (let (buffer-read-only)
+      (forward-line 2) ;; beyond dir. header
+      (sort-regexp-fields t "^.*$" "[ ]*." (point) (point-max)))
+    (set-buffer-modified-p nil)))
+
+(defadvice dired-readin
+  (after dired-after-updating-hook first () activate)
+  "Sort dired listings with directories first before adding marks."
+  'dm/dired-sort)
+
+;; - Get a warning on the modeline when editing a file as root
+(defun dm/dired-mode-line-function ()
+  (when (string-match "^/su\\(do\\)?:" default-directory)
+    (setq mode-line-format
+          (format-mode-line mode-line-format 'font-lock-warning-face))))
+
+;; -----------------
+;; kill all dired buffers
+;; -----------------
+(defun kill-all-dired-buffers()
+      "Kill all dired buffers."
+      (interactive)
+      (save-excursion
+        (let((count 0))
+          (dolist(buffer (buffer-list))
+            (set-buffer buffer)
+            (when (equal major-mode 'dired-mode)
+              (setq count (1+ count))
+              (kill-buffer buffer)))
+          (message "Killed %i dired buffer(s)." count ))))
+
+
 (use-package dired-subtree
   :config
   (bind-keys :map dired-mode-map
@@ -53,3 +100,13 @@
   :config
   (bind-keys :map dired-mode-map
              ("C-c C-f" . dired-filter-mode)))
+
+;; This is a minor mode that can be enabled from a dired buffer.
+;; Once enabled it will show the file from point in the other window.
+;; Moving to the other file within the dired buffer with <down>/<up> or
+;; C-n/C-p will display different file.
+;; Hitting <SPC> will scroll the peeped file down, whereas
+;; C-<SPC> and <backspace> will scroll it up.
+(use-package peep-dired
+  :config
+  (define-key dired-mode-map (kbd "C-c C-p") 'peep-dired))
