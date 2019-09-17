450d8dc8715237a7cee6d327bb08c25c9ba5f7a9
David Martinez <dm@dmartinez.net>
2019-09-17 12:26:37 -0400

    Updates to org mode config
    * Let windmove work in more places
    * Re-enable src-block shortcuts post org ~9.3
    * Add new shortcut types
    * Add org-capture shortcut

--- .emacs.d/init.d/605-org-mode.el
+++ .emacs.d/init.d/605-org-mode.el
@@ -22,25 +22,43 @@
   (setq org-fontify-whole-heading-line t)
   (setq org-log-into-drawer "NOTES")
   (setq org-refile-allow-creating-parent-nodes t)
-
   (setq org-babel-no-eval-on-ctrl-c-ctrl-c nil)
   (setq org-confirm-babel-evaluate nil)
+
+  ; babel blocks
+  (setq org-src-tab-acts-natively t)
+
+  ; capture
+  (setq org-default-notes-file "~/org/notes.org")
 
   ;;(setq org-startup-indented t)
   (setf org-blank-before-new-entry '((heading . nil) (plain-list-item . nil)))
   (org-babel-do-load-languages 'org-babel-load-languages dm/org-babel-languages)
 
   :bind (
-         ("C-c C-TAB" . dm/cycle-previous-heading)
+         ("C-c C-TAB" . dm/cycle-previous-heading) ; not working
          )
   )
+
+;; Make windmove work in Org mode if shift-arrow won't
+;; do anything special
+(add-hook 'org-shiftup-final-hook    'windmove-up)
+(add-hook 'org-shiftleft-final-hook  'windmove-left)
+(add-hook 'org-shiftdown-final-hook  'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
+;; global bindings for jumping into org-mode
+; (global-set-key (kbd "C-c l") 'org-store-link)
+; (global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
 
 (use-package org-tempo
   :defer org-load-defer-seconds
   :init
-  (add-to-list 'org-structure-template-alist '("ex" . "src elixir"))
-  (add-to-list 'org-structure-template-alist '("ru" . "src ruby"))
-  (add-to-list 'org-structure-template-alist '("sh" . "src bash"))
+  (add-to-list 'org-structure-template-alist '("ex"  . "src elixir"))
+  (add-to-list 'org-structure-template-alist '("ru"  . "src ruby"))
+  (add-to-list 'org-structure-template-alist '("sh"  . "src bash"))
+  (add-to-list 'org-structure-template-alist '("sql" . "src sql"))
   )
 
 (use-package org-bullets
