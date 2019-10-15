73cda84407bf18640df530e2dcbfdeb28a2cb083
David Martinez <dm@dmartinez.net>
2019-10-15 13:21:57 -0400

    Clean advice file

--- .emacs.d/my-misc-advice.el
+++ .emacs.d/my-misc-advice.el
@@ -1,24 +1,4 @@
-;;; Various advice functions that haven't a home elsewhere
-
-
-;;; Disabled for now - trying out avy-zap
-;; When using zap-to char, don't delete the actual target char.
-;; I.e. make zap-to-char be zap-up-to-char
-; (defadvice zap-to-char (after my-zap-to-char-advice (arg char) activate)
-;   "Kill up to the ARG'th occurence of CHAR, and leave CHAR. If
-;   you are deleting forward, the CHAR is replaced and the point is
-;   put before CHAR"
-;   (insert char)
-;   (if (< 0 arg) (forward-char -1)))
-
-;; When using zap-to char, don't delete the actual target char.
-;; I.e. make zap-to-char be zap-up-to-char
-(defadvice icicle-zap-to-char (after my-zap-to-char-advice (arg char) activate)
-  "Kill up to the ARG'th occurence of CHAR, and leave CHAR. If
-  you are deleting forward, the CHAR is replaced and the point is
-  put before CHAR"
-  (insert char)
-  (if (< 0 arg) (forward-char -1)))
+;;; Various advice functions
 
 ;; When using open-line, move to the beginning-of-line first
 ;; (i.e. don't split the current line)
