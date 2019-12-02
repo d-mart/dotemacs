7d0a1051f570751e97d60bdf6bd9fab9375166f2
David Martinez <dmartinez@dm.tld>
2019-12-02 08:00:13 -0500

    Add python config file

---
+++ .emacs.d/init.d/650-python-setup.el
@@ -0,0 +1,20 @@
+;;;
+;;; Configuration and mode customization for python
+;;;
+
+;; python-mode keybindings
+(defun dm/python-mode-keybindings ()
+  (define-key ruby-mode-map (kbd "C-c C-c") 'comment-or-uncomment-region)
+  (define-key ruby-mode-map (kbd "C-c C-d") 'dm/python-insert-debugger))
+
+(add-hook 'python-mode-hook 'dm/programming-mode-hook)
+(add-hook 'python-mode-hook 'dm/python-mode-keybindings)
+(add-hook 'python-mode-hook 'fontify-at-todo)
+
+(defun dm/python-insert-debugger ()
+  (interactive)
+  (beginning-of-line)
+  (open-line 1)
+  (indent-for-tab-command)
+  (insert "import pdb; pdb.set_trace() #XXXXXXXXXXXXXXXXXX")
+  )
