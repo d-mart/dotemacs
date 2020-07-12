30f94a613c6be2ab518433ed684016ed2727acd8
David Martinez <dm@dmartinez.net>
2020-07-11 23:56:41 -0400

    Update plantuml settings

--- .emacs.d/init.d/450-plantuml-mode.el
+++ .emacs.d/init.d/450-plantuml-mode.el
@@ -1,4 +1,5 @@
 (use-package plantuml-mode
   :init
+  (setq plantuml-default-exec-mode 'jar) ; or 'server if you want diagrams sent out
   (setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2019.8/libexec/plantuml.jar")
   )
