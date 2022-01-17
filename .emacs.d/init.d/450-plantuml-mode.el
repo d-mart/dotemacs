335a6d628698cc1369390a56f46ba53da40e2d1e
David Martinez <dm@dmartinez.net>
2022-01-17 10:43:52 -0500

    Update plantuml settings

--- .emacs.d/init.d/450-plantuml-mode.el
+++ .emacs.d/init.d/450-plantuml-mode.el
@@ -2,4 +2,5 @@
   :init
   (setq plantuml-default-exec-mode 'jar) ; or 'server if you want diagrams sent out
   (setq plantuml-jar-path "/usr/local/Cellar/plantuml/1.2021.10/libexec/plantuml.jar")
+  (setq plantuml-indent-level 2)
   )
