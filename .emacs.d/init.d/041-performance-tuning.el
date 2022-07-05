0b1c0b30affe44b81f453af0435098c6b4388cc4
David Martinez <dmartinez@dm.tld>
2022-07-05 08:30:30 -0400

    Performance updates for long lines

--- .emacs.d/init.d/041-performance-tuning.el
+++ .emacs.d/init.d/041-performance-tuning.el
@@ -6,3 +6,8 @@
 
 ;; Avoid performance issues in files with very long lines.
 (global-so-long-mode 1)
+
+;; more long-line tweaks
+; https://emacs.stackexchange.com/questions/598/how-do-i-prevent-extremely-long-lines-making-emacs-slow
+(setq bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)
