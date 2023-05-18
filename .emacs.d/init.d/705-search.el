814943cea0ef7c4350fe496be0afa7dcc0737341
David Martinez <dmartinez@dm.tld>
2023-05-18 13:36:35 -0400

    Switch to urgrep for flexibility in greppy things

--- .emacs.d/init.d/705-search.el
+++ .emacs.d/init.d/705-search.el
@@ -6,7 +6,15 @@
   :init
   (setq deadgrep--search-type 'regexp)
   :bind
-  ("C-h g" . deadgrep))
+  ("C-h G" . deadgrep))
 
 ;; rgrep - friendly recursive grep function
 (global-set-key (kbd "C-c g") 'rgrep)
+
+;; use any available grep-like tool
+;; https://github.com/jimporter/urgrep
+(use-package urgrep
+  :init
+  (setq urgrep-search-regexp t)
+  :bind
+  ("C-h g" . urgrep))
