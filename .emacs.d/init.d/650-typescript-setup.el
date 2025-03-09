72da85b39f71cc6a0509bc5e3d225e7ea56c9987
David Martinez <dmartinez@dm.tld>
2025-03-09 12:10:32 -0400

    Update typescript setup

--- .emacs.d/init.d/650-typescript-setup.el
+++ .emacs.d/init.d/650-typescript-setup.el
@@ -3,9 +3,19 @@
 ;;; ----------------------
 (use-package typescript-mode)
 
-(defun dm/ts-mode-settings ())
+(defun ts-insert-semicolon-and-newline ()
+  "Move to the end of the current line, add a semicolon if one isn't present, and open a new line below."
+  (interactive)
+  (end-of-line)
+  (unless (looking-back ";" 1)
+    (insert ";"))
+  (newline-and-indent))
 
-(defun dm/ts-mode-keybindings ())
+(defun dm/ts-mode-settings ()
+  (setq typescript-indent-level 2))
+
+(defun dm/ts-mode-keybindings ()
+  (define-key typescript-mode-map (kbd "M-<return>") 'ts-insert-semicolon-and-newline))
 
 (add-hook 'typescript-mode-hook 'dm/programming-mode-hook)
 (add-hook 'typescript-mode-hook 'dm/programming-mode-keybindings)
